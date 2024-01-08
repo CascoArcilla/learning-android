c1575ff41521d8edd0355eb1f82bb59a64c933bc
CascoArcilla <normon1230@gmail.com>
2024-01-07 19:26:14 -0600

    Created imcapp package

--- app/src/main/java/com/cascoArcilla/courseadroid/MenuActivity.kt
+++ app/src/main/java/com/cascoArcilla/courseadroid/MenuActivity.kt
@@ -1,22 +1,36 @@
 package com.cascoArcilla.courseadroid
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import com.cascoArcilla.courseadroid.firstapp.FirstAppActivity
+import com.cascoArcilla.courseadroid.imcapp.ImcCalculatorActivity
 
 class MenuActivity : AppCompatActivity() {
+    private val APPS = listOf<String>("saludo", "imc")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
         val buttonAppSaludar = findViewById<Button>(R.id.btnAppSaludo)
-        buttonAppSaludar.setOnClickListener { navigateToSaludarApp() }
+        buttonAppSaludar.setOnClickListener { navigateTo(APPS[0]) }
+
+        val buttonAppImc = findViewById<Button>(R.id.btnAppImc)
+        buttonAppImc.setOnClickListener { navigateTo(APPS[1]) }
     }
 
-    fun navigateToSaludarApp(){
-        val intent = Intent(this, FirstAppActivity::class.java)
+    private fun navigateTo(app:String){
+        var intent : Intent
+
+        when(app){
+            APPS.get(0) -> intent = Intent(this, FirstAppActivity::class.java)
+            APPS.get(1) -> intent = Intent(this, ImcCalculatorActivity::class.java)
+        }
+
         startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/cascoArcilla/courseadroid/imcapp/ImcCalculatorActivity.kt
@@ -0,0 +1,12 @@
+package com.cascoArcilla.courseadroid.imcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cascoArcilla.courseadroid.R
+
+class ImcCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc_calculator)
+    }
+}
