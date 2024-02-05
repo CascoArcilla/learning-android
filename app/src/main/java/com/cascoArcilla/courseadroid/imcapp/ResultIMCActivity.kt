7c19b857658bb7a7ed7b380e46ac4596d80192ee
CascoArcilla <normon1230@gmail.com>
2024-02-05 14:48:39 -0600

    Showed imc into ResultIMCActivity and added back button since mainfest

--- app/src/main/java/com/cascoArcilla/courseadroid/imcapp/ResultIMCActivity.kt
+++ app/src/main/java/com/cascoArcilla/courseadroid/imcapp/ResultIMCActivity.kt
@@ -1,15 +1,42 @@
 package com.cascoArcilla.courseadroid.imcapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.cascoArcilla.courseadroid.R
 import com.cascoArcilla.courseadroid.imcapp.ImcCalculatorActivity.Companion.KEY_IMC
 
 class ResultIMCActivity : AppCompatActivity() {
+    private var imc: Float = 0.0f
+
+    private lateinit var tvResultImcNumber: TextView
+    private lateinit var btnBackCalculatorImc: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result_imcactivity)
 
-        val imc = intent.extras?.getFloat(KEY_IMC) ?: -1.0f
+        initComponents()
+        addListeners()
+        showNumberImc()
+    }
+
+    private fun initComponents() {
+        imc = intent.extras?.getFloat(KEY_IMC) ?: -1.0f
+        tvResultImcNumber = findViewById(R.id.tvResultImcNumber)
+        btnBackCalculatorImc = findViewById(R.id.btnBackImcCalculator)
+    }
+
+    private fun addListeners() {
+        btnBackCalculatorImc.setOnClickListener {
+            var intentBackToImc = Intent(this, ImcCalculatorActivity::class.java)
+            startActivity(intentBackToImc)
+        }
+    }
+
+    private fun showNumberImc() {
+        tvResultImcNumber.text = imc.toString()
     }
 }
